60e1d149ed8bf34e9f82a79917dd459515e3e5f5
yashm <yshmarov@gmail.com>
2018-05-15 20:30:22 +0000

    scaffold ideas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
   devise_for :users
   root to: "home#index"
+  get 'home/index'
+  resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
