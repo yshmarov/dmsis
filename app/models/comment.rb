201bdf955a3210ad7348ad51844bc4ba19da3407
yashm <yshmarov@gmail.com>
2018-05-16 09:30:12 +0000

    comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :idea
   belongs_to :user
+  validates :user_id, :idea_id, :body, presence: :true
+  def to_s
+    body
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users
   root to: "home#index"
   get 'home/index'
   resources :ideas
+  resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
