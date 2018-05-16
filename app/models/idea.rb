70ed1edc6b6b259ce540086e5963cc9c5074657d
yashm <yshmarov@gmail.com>
2018-05-16 09:57:25 +0000

    better voting buttons

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,6 +3,9 @@ class Idea < ApplicationRecord
   has_many :comments
   validates :user_id, :name, :description, presence: :true
   acts_as_votable
+  def total_score
+    positive_score - negative_score
+  end
   def positive_score
     self.get_upvotes.size
   end	
