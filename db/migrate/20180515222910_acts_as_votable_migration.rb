a7ae915d3fe1e798367adf0604356f26c9fd69e3
yashm <yshmarov@gmail.com>
2018-05-17 11:08:32 +0000

    update migration of acts_as_votable to rails 5.2

--- db/migrate/20180515222910_acts_as_votable_migration.rb
+++ db/migrate/20180515222910_acts_as_votable_migration.rb
@@ -1,4 +1,4 @@
-class ActsAsVotableMigration < ActiveRecord::Migration[4.2]
+class ActsAsVotableMigration < ActiveRecord::Migration[5.2]
   def self.up
     create_table :votes do |t|
 
--- db/schema.rb
+++ db/schema.rb
@@ -73,18 +73,20 @@ ActiveRecord::Schema.define(version: 2018_05_16_205313) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "votes", id: :integer, default: nil, force: :cascade do |t|
+  create_table "votes", force: :cascade do |t|
     t.string "votable_type"
-    t.integer "votable_id"
+    t.bigint "votable_id"
     t.string "voter_type"
-    t.integer "voter_id"
+    t.bigint "voter_id"
     t.boolean "vote_flag"
     t.string "vote_scope"
     t.integer "vote_weight"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["votable_type", "votable_id"], name: "index_votes_on_votable_type_and_votable_id"
     t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
+    t.index ["voter_type", "voter_id"], name: "index_votes_on_voter_type_and_voter_id"
   end
 
   add_foreign_key "comments", "ideas"
