4561d4642910036e8e135d9431366a1280355ea0
yashm <yshmarov@gmail.com>
2018-05-21 18:02:11 +0000

    user can sign in with facebook even if he did not provide an email

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,7 +52,11 @@ class User < ApplicationRecord
       user
     else
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.email = auth.info.email
+        if auth.info.email.present?
+          user.email = auth.info.email
+        else
+          user.email = "#{auth.uid}@facebook.com"
+        end
         user.password = Devise.friendly_token[0,20]
         user.name = auth.info.name   # assuming the user model has a name
         user.image = auth.info.image # assuming the user model has an image
