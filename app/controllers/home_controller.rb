04e953f5d3a0e0d6554f7bad14d31285ccc8329d
yashm <yshmarov@gmail.com>
2018-06-13 15:44:01 +0000

    make background dark and change font to white

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, :only => [ :index, :privacy_policy, :terms_of_service, :contacts ]
 
   def index
-    @ideas = Idea.order("created_at DESC").limit(5)
+    @ideas = Idea.order("created_at DESC").limit(3)
     if current_user
       redirect_to top_ideas_path
     end
