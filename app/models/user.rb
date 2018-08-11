a2c9bd0b994c275dbd0b292a31484f702ac4a517
yashm <yshmarov@gmail.com>
2018-08-11 11:06:28 +0000

    revert previous commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   #User.find_each { |user| User.reset_counters(user.id, :cofounders) }
   #User.find_each { |user| User.reset_counters(user.id, :favorites) }
 
-  #devise :database_authenticatable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
-  #devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+  #devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :confirmable, :omniauth_providers => [:facebook]
 
   acts_as_voter
   has_many :ideas, dependent: :destroy
