91e5dfb60cf21c495235d685fd6524b5668ec114
yashm <yshmarov@gmail.com>
2018-08-18 08:51:14 +0000

    rename feed to recent_activity

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, :only => [ :privacy_policy, :terms_of_service, :contacts ]
 
-  def feed
+  def recent_activity
     @activities = PublicActivity::Activity.all.reverse
   end
 
