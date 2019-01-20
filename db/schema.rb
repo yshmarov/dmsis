b69bc402964d0d303119c3e1c328a21762219f64
yashm <yshmarov@gmail.com>
2019-01-20 02:00:03 +0000

    bundle update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_205621) do
+ActiveRecord::Schema.define(version: 2018_11_18_123016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -154,6 +154,10 @@ ActiveRecord::Schema.define(version: 2018_08_29_205621) do
     t.integer "cofounders_count", default: 0, null: false
     t.integer "favorites_count", default: 0, null: false
     t.integer "points", default: 0, null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
